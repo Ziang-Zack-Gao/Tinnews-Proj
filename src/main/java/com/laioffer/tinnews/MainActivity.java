030bc793ee1e0d1d7db0f058c70217986352ee97
Ziang (Zack) Gao <68089542+Ziang-Zack-Gao@users.noreply.github.com>
2020-07-18 17:47:18 -0400

    Update MainActivity.java

--- src/main/java/com/laioffer/tinnews/MainActivity.java
+++ src/main/java/com/laioffer/tinnews/MainActivity.java
@@ -3,11 +3,20 @@ package com.laioffer.tinnews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.laioffer.tinnews.model.NewsResponse;
+import com.laioffer.tinnews.network.NewsApi;
+import com.laioffer.tinnews.network.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,16 +27,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-               BottomNavigationView navView = findViewById(R.id.nav_view);
-               NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                       .findFragmentById(R.id.nav_host_fragment);
-               navController = navHostFragment.getNavController();
-               NavigationUI.setupWithNavController(navView, navController);
-               NavigationUI.setupActionBarWithNavController(this, navController);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController);
     }
 
-   @Override
-   public boolean onSupportNavigateUp() {
-               return navController.navigateUp();
-           }
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp();
+    }
 }
